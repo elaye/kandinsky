b1b99d8328dc0a678629eb28a5481d0885598c3a
Elie Génard <genard.elie@orange.fr>
2014-06-06 17:37:45 +0200

    Add HSV getters

--- lib/kandinsky/color.rb
+++ lib/kandinsky/color.rb
@@ -12,7 +12,33 @@ class Color
 		@b = [[0, b].max, 255].min
 	end
 
-	public
+	def h
+		max = [@r, @g, @b].max
+		min = [@r, @g, @b].min
+		if max == min
+			0
+		elsif max == @r
+			(60*(@g - @b)/(max - min) + 360) % 360
+		elsif max == @g	
+			60*(@b - @r)/(max - min) + 120
+		elsif max == @b
+			60*(@r - @g)/(max - min) + 240
+		end
+	end
+
+	def s
+		max = [@r, @g, @b].max
+		min = [@r, @g, @b].min
+		if max == 0
+			0
+		else
+			((1 - min.to_f/max.to_f)*100).to_i		
+		end
+	end
+
+	def v
+		([@r, @g, @b].max.to_f / 255 * 100).to_i
+	end
 
 	def to_str
 		"[#{@r}, #{@g}, #{@b}]"
--- test/test_kandinsky.rb
+++ test/test_kandinsky.rb
@@ -3,6 +3,10 @@ require 'kandinsky'
 
 class KandinskyTest < Test::Unit::TestCase
 	
+	def setup
+		@c = Color.new(137, 6, 39)
+	end
+
 	def test_color_init_default
 		assert_kind_of Color,
 			Color.new
@@ -30,6 +34,27 @@ class KandinskyTest < Test::Unit::TestCase
 	def test_color_to_str
 		assert_equal "[137, 6, 39]",
 			Color.new(137, 6, 39).to_str
+	end
+
+	def test_color_set_rgb
+		@c.r = 25
+		assert_equal 25,
+			@c.r
+		@c.g = 123
+		assert_equal 123,
+			@c.g
+		@c.b = 73
+		assert_equal 73,
+			@c.b
+	end
+
+	def test_color_get_hsv
+		assert_equal 344,
+			@c.h
+		assert_equal 95,
+			@c.s
+		assert_equal 53,
+			@c.v
 	end
 
 end
